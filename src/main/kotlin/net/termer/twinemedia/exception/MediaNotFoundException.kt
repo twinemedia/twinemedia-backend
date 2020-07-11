40a6a8215569b40115648941b2f8d93badd0105d
= <termer@protonmail.com>
2020-07-11 14:02:10 -0400

    fix message not being passed to Exception super constructor

--- src/main/kotlin/net/termer/twinemedia/exception/MediaNotFoundException.kt
+++ src/main/kotlin/net/termer/twinemedia/exception/MediaNotFoundException.kt
@@ -4,6 +4,6 @@ package net.termer.twinemedia.exception
  * Exception to be thrown when a media entry cannot be found
  * @since 1.0
  */
-class MediaNotFoundException(msg : String?) : Exception() {
+class MediaNotFoundException(msg : String?) : Exception(msg) {
     // Nothing else needs to be added
 }
--- src/main/kotlin/net/termer/twinemedia/exception/WrongMediaTypeException.kt
+++ src/main/kotlin/net/termer/twinemedia/exception/WrongMediaTypeException.kt
@@ -4,6 +4,6 @@ package net.termer.twinemedia.exception
  * Exception to be thrown when a media file is not the desired type
  * @since 1.0
  */
-class WrongMediaTypeException(msg : String?) : Exception() {
+class WrongMediaTypeException(msg : String?) : Exception(msg) {
     // Nothing else needs to be added
 }
