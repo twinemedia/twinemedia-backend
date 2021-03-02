cb6c6673d51861e6497846973369b3e34cbd3d17
= <termer@protonmail.com>
2021-03-02 16:59:03 -0500

    move config wizards to separate file

---
+++ src/main/kotlin/net/termer/twinemedia/InteractiveConfiguration.kt
@@ -0,0 +1,475 @@
+package net.termer.twinemedia
+
+import io.vertx.core.json.Json
+import kotlinx.coroutines.runBlocking
+import net.termer.twine.utils.StringFilter
+import net.termer.twine.utils.files.BlockingReader
+import net.termer.twine.utils.files.BlockingWriter
+import net.termer.twinemedia.db.dbInit
+import net.termer.twinemedia.db.dbMigrate
+import net.termer.twinemedia.model.AccountsModel
+import net.termer.twinemedia.util.createAccount
+import net.termer.twinemedia.util.updateAccountPassword
+import net.termer.twinemedia.util.validEmail
+import java.io.File
+
+// System console
+private val cons = System.console()
+
+// Instantiated models
+private val accountsModel = AccountsModel()
+
+// Notice about Kotlin coroutine calls:
+// Coroutine calls are NOT called inside a Vert.x context because blocking calls are done inside of them, intentionally.
+// This would both block those threads for normal operation by other parts of the application, and additional throw exceptions because of being blocked.
+
+/**
+ * Admin creation prompt
+ * @since 1.4.0
+ */
+private fun createAdminPrompt() {
+	var email = ""
+	while(email.isBlank()) {
+		print("Email: ")
+		val addr = cons.readLine()
+
+		if(validEmail(addr)) {
+			runBlocking {
+				// Check if account with that email already exists
+				val emailRes = accountsModel.fetchAccountByEmail(addr)
+
+				if(emailRes.count() > 0)
+					println("Account with that email already exists")
+				else
+					email = addr
+			}
+		} else {
+			println("!!! Invalid email address !!!")
+		}
+	}
+
+	print("Name: ")
+	val name = cons.readLine()
+
+	print("Password: ")
+	val pass = cons.readPassword().joinToString("")
+
+	println("Creating account...")
+	runBlocking {
+		createAccount(name, email, true, arrayOf(), pass)
+	}
+	println("Created!")
+}
+
+/**
+ * Runs an interactive install wizard in the console
+ * @since 1.4.0
+ */
+fun interactiveInstall() {
+	val cfg = File("configs/twinemedia.json")
+	if(cfg.exists()) {
+		Module.config = Json.decodeValue(BlockingReader.read(cfg), TwineMediaConfig::class.java)
+		if(!Module.config.upload_location.endsWith('/'))
+			Module.config.upload_location += '/'
+	}
+
+	println("Welcome to TwineMedia. You are about to install and configure this module.")
+	println("Press enter to continue.")
+	var ln = cons.readLine()
+	print("Show advanced install options? [y/N]: ")
+	val advanced = cons.readLine()?.toLowerCase()?.startsWith("y") == true
+
+	println("Which Twine domain do you want TwineMedia to run on (can select \"*\" for all)? (${Module.config.domain}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.domain = ln.trim()
+
+	println("What directory do you want uploaded files to be stored? (${Module.config.upload_location}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.upload_location = ln.trim()
+
+	// Check if dir exists, if not, ask to create
+	val dir = File(Module.config.upload_location)
+	if(!dir.exists()) {
+		println("Directory \"${Module.config.upload_location}\" doesn't exist, create it? [Y/n]: ")
+
+		if(cons.readLine()?.toLowerCase()?.startsWith("n") == false)
+			dir.mkdirs()
+	}
+
+	var size: Int? = null
+	while(size == null) {
+		println("What maximum upload size do you want (in MB)? (${Module.config.max_upload / 1024 / 1024}): ")
+		ln = cons.readLine()
+
+		if(ln != null && ln.trim().isNotBlank()) {
+			try {
+				size = ln.trim().toInt() * 1024 * 1024
+			} catch(e: Exception) {
+				println("!!! Value must be an integer !!!")
+			}
+		} else {
+			size = Module.config.max_upload
+		}
+	}
+	Module.config.max_upload = size
+
+	var jwtSecret = StringFilter.generateString(32)
+
+	if(advanced) {
+		println("What should the JWT secret key be? ($jwtSecret): ")
+		ln = cons.readLine()
+
+		if(ln != null && ln.isNotBlank())
+			jwtSecret = ln
+
+		var jwtExpire: Int? = null
+		while(jwtExpire == null) {
+			println("In how many minutes should JWT tokens expire? (${Module.config.jwt_expire_minutes}): ")
+			ln = cons.readLine()
+
+			if(ln != null && ln.trim().isNotBlank()) {
+				try {
+					jwtExpire = ln.trim().toInt()
+				} catch(e: Exception) {
+					println("!!! Value must be an integer !!!")
+				}
+			} else {
+				jwtExpire = Module.config.jwt_expire_minutes
+			}
+		}
+		Module.config.jwt_expire_minutes = jwtExpire
+	}
+
+	Module.config.jwt_secret = jwtSecret
+
+	println("What address is the PostgreSQL database running on? (${Module.config.db_address}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.db_address
+
+	var dbPort: Int? = null
+	while(dbPort == null) {
+		println("What port is the PostgreSQL database running on? (${Module.config.db_port}): ")
+		ln = cons.readLine()
+
+		if(ln != null && ln.trim().isNotBlank()) {
+			try {
+				dbPort = ln.trim().toInt()
+			} catch(e: Exception) {
+				println("!!! Value must be an integer !!!")
+			}
+		} else {
+			dbPort = Module.config.db_port
+		}
+	}
+	Module.config.db_port = dbPort
+
+	println("What is the name of the database TwineMedia will use? (${Module.config.db_name}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.db_name = ln.trim()
+
+	println("What username are you using to authenticate with the database? (${Module.config.db_user}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.db_user = ln.trim()
+
+	println("What password are you using to authenticate with the database? (<leave blank to use existing/default value>): ")
+	ln = cons.readPassword().joinToString("")
+
+	if(ln.isNotBlank())
+		Module.config.db_pass = ln
+
+	if(advanced) {
+		var dbPool: Int? = null
+		while(dbPool == null) {
+			println("What should the max database connection pool size be? (${Module.config.db_max_pool_size}): ")
+			ln = cons.readLine()
+
+			if(ln != null && ln.trim().isNotBlank()) {
+				try {
+					dbPool = ln.trim().toInt()
+				} catch(e: Exception) {
+					println("!!! Value must be an integer !!!")
+				}
+			} else {
+				dbPool = Module.config.db_max_pool_size
+			}
+		}
+		Module.config.db_max_pool_size = dbPool
+
+		println("Should migrations automatically be run when TwineMedia starts? [Y/n]: ")
+		Module.config.db_auto_migrate = cons.readLine()?.startsWith("n") == false
+
+		var cryptCount: Int? = null
+		while(cryptCount == null) {
+			println("How many processors should be used to hash passwords? (${Module.config.crypt_processor_count}): ")
+			ln = cons.readLine()
+
+			if(ln != null && ln.trim().isNotBlank()) {
+				try {
+					cryptCount = ln.trim().toInt()
+				} catch(e: Exception) {
+					println("!!! Value must be an integer !!!")
+				}
+			} else {
+				cryptCount = Module.config.crypt_processor_count
+			}
+		}
+		Module.config.crypt_processor_count = cryptCount
+
+		var cryptMem: Int? = null
+		while(cryptMem == null) {
+			println("How many kilobytes of memory should be used to hash passwords? (${Module.config.crypt_memory_kb}): ")
+			ln = cons.readLine()
+
+			if(ln != null && ln.trim().isNotBlank()) {
+				try {
+					cryptMem = ln.trim().toInt()
+				} catch(e: Exception) {
+					println("!!! Value must be an integer !!!")
+				}
+			} else {
+				cryptMem = Module.config.crypt_memory_kb
+			}
+		}
+		Module.config.crypt_memory_kb = cryptMem
+
+		println("What host will the frontend be running on? (${Module.config.frontend_host}): ")
+		ln = cons.readLine()
+
+		if(ln != null && ln.trim().isNotBlank())
+			Module.config.frontend_host = ln.trim()
+	}
+
+	println("Will TwineMedia be running behind a reverse proxy (such as Nginx)? [y/N]: ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.reverse_proxy = ln.toLowerCase().startsWith("n") == false
+
+	println("Where is ffmpeg located? (${Module.config.ffmpeg_path}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.ffmpeg_path = ln.trim()
+
+	println("Where is ffprobe located? (${Module.config.ffprobe_path}): ")
+	ln = cons.readLine()
+
+	if(ln != null && ln.trim().isNotBlank())
+		Module.config.ffprobe_path = ln.trim()
+
+	var procCount: Int? = null
+	while(procCount == null) {
+		println("How many files should TwineMedia be able to process at once? (${Module.config.media_processor_count}): ")
+		ln = cons.readLine()
+
+		if(ln != null && ln.trim().isNotBlank()) {
+			try {
+				procCount = ln.trim().toInt()
+			} catch(e: Exception) {
+				println("!!! Value must be an integer !!!")
+			}
+		} else {
+			procCount = Module.config.media_processor_count
+		}
+	}
+	Module.config.media_processor_count = procCount
+
+	if(advanced) {
+		var authMax: Int? = null
+		while(authMax == null) {
+			println("How many attempts can a user make to authenticate before they will be throttled? (${Module.config.max_auth_attempts}): ")
+			ln = cons.readLine()
+
+			if(ln != null && ln.trim().isNotBlank()) {
+				try {
+					authMax = ln.trim().toInt()
+				} catch(e: Exception) {
+					println("!!! Value must be an integer !!!")
+				}
+			} else {
+				authMax = Module.config.max_auth_attempts
+			}
+		}
+		Module.config.max_auth_attempts = authMax
+
+		var authTimeout: Int? = null
+		while(authTimeout == null) {
+			println("How many milliseconds will users be throttled after hitting their max auth attempts? (${Module.config.auth_timeout_period}): ")
+			ln = cons.readLine()
+
+			if(ln != null && ln.trim().isNotBlank()) {
+				try {
+					authTimeout = ln.trim().toInt()
+				} catch(e: Exception) {
+					println("!!! Value must be an integer !!!")
+				}
+			} else {
+				authTimeout = Module.config.auth_timeout_period
+			}
+		}
+		Module.config.auth_timeout_period = authTimeout
+	}
+
+	// Check if config exists, ask for overwrite confirmation if it does
+	if(cfg.exists()) {
+		println("A configuration file already exists, replace it? [y/N]: ")
+		if(cons.readLine().toLowerCase().startsWith("y")) {
+			BlockingWriter.write("configs/twinemedia.json", Json.encodePrettily(Module.config))
+		}
+	} else {
+		BlockingWriter.write("configs/twinemedia.json", Json.encodePrettily(Module.config))
+	}
+
+	runBlocking {
+		// Connect to database
+		println("Testing database connection...")
+		try {
+			dbInit()
+			println("Database connection successful!")
+			println("Setting up schema...")
+			dbMigrate()
+			println("Checking for an admin account...")
+			val admins = accountsModel.fetchAdminAccounts()
+			if(admins.count() > 0) {
+				println("An admin account already exists")
+			} else {
+				println("No admin account exists")
+			}
+
+			println("Would you like to create one? [Y/n]: ")
+			if(cons.readLine().toLowerCase().startsWith("y")) {
+				createAdminPrompt()
+			}
+		} catch(e: Exception) {
+			System.err.println("Error occurred when attempting to access database, check your database settings. Error:")
+			e.printStackTrace()
+		}
+	}
+
+	println("Installation and configuration complete!")
+}
+
+/**
+ * Runs an interactive administrator password reset wizard in the console
+ * @since 1.4.0
+ */
+fun interactiveResetAdminPassword() {
+	val cfg = File("configs/twinemedia.json")
+	if(cfg.exists()) {
+		Module.config = Json.decodeValue(BlockingReader.read(cfg), TwineMediaConfig::class.java)
+		if(!Module.config.upload_location.endsWith('/'))
+			Module.config.upload_location += '/'
+	} else {
+		println("TwineMedia is not configured, please run Twine with the --twinemedia-install option to configure and install everything")
+		return
+	}
+
+	println("Welcome to TwineMedia. You are about to reset the password of an administrator account.")
+	println("Press enter to continue.")
+	cons.readLine()
+
+	println("Attempting to connect to the database...")
+	dbInit()
+	println("Connected!")
+
+	runBlocking {
+		println("Fetching admin accounts...")
+		val adminsRes = accountsModel.fetchAdminAccounts()
+
+		if(adminsRes.count() > 0) {
+			println("Select the ID of the account you want to reset the password of:")
+
+			var idColWidth = 2
+			var nameColWidth = 4
+			var emailColWidth = 5
+
+			for(acc in adminsRes) {
+				if(acc.id.toString().length > idColWidth)
+					idColWidth = acc.id.toString().length
+				if(acc.name.length > nameColWidth)
+					nameColWidth = acc.name.length
+				if(acc.email.length > emailColWidth)
+					emailColWidth = acc.email.length
+			}
+
+			// Print columns
+			print("ID")
+			repeat(idColWidth - 2) { print(' ') }
+			print(" | Name")
+			repeat(nameColWidth - 4) { print(' ') }
+			print(" | Email")
+			repeat(emailColWidth - 5) { print(' ') }
+			println()
+
+			for(acc in adminsRes) {
+				val id = acc.id.toString()
+				val name = acc.name
+				val email = acc.email
+
+				// Print row
+				print(id)
+				repeat(idColWidth - id.length) { print(' ') }
+				print(" | $name")
+				repeat(nameColWidth - name.length) { print(' ') }
+				print(" | $email")
+				repeat(emailColWidth - email.length) { print(' ') }
+				println()
+			}
+
+			try {
+				print("Account ID: ")
+				val id = cons.readLine().toInt()
+
+				// Check if account is in list
+				var exists = false
+				for(acc in adminsRes)
+					if(acc.id == id) {
+						exists = true
+						break
+					}
+
+				if(exists) {
+					print("New password: ")
+					val pass = cons.readPassword().joinToString("")
+					print("Confirm password: ")
+					val passConf = cons.readPassword().joinToString("")
+
+					// Validation
+					when {
+						pass.isEmpty() -> println("!!! Passwords cannot be blank !!!")
+						pass == passConf -> {
+							try {
+								println("Updating password...")
+								updateAccountPassword(id, pass)
+								println("Updated!")
+							} catch(e: Exception) {
+								System.err.println("Failed to update password because of error:")
+								e.printStackTrace()
+							}
+						}
+						else -> println("!!! Passwords do not match !!!")
+					}
+				} else {
+					println("!!! Invalid ID !!!")
+				}
+			} catch(e: Exception) {
+				println("!!! Invalid ID !!!")
+			}
+		} else {
+			println("No admin accounts exist, would you like to create one? [Y/n]: ")
+			if(cons.readLine().toLowerCase().startsWith("y")) {
+				createAdminPrompt()
+			}
+		}
+	}
+}
