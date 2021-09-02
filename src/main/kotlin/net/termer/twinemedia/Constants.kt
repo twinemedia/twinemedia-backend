3744a070dd7437d2836ea1ea2713ce76d9bb45ce
= <termer@protonmail.com>
2021-09-02 15:36:24 -0400

    use a normal object instead of companion, and use const for string

--- src/main/kotlin/net/termer/twinemedia/Constants.kt
+++ src/main/kotlin/net/termer/twinemedia/Constants.kt
@@ -4,18 +4,16 @@ package net.termer.twinemedia
  * Constants for this module, mostly relating to versions and API support
  * @since 1.4.1
  */
-class Constants {
-	companion object {
-		/**
-		 * The module version
-		 * @since 1.4.1
-		 */
-		val VERSION = "1.5.1"
+object Constants {
+	/**
+	 * The module version
+	 * @since 1.4.1
+	 */
+	const val VERSION = "1.5.2"
 
-		/**
-		 * The supported API versions
-		 * @since 1.4.1
-		 */
-		val API_VERSIONS = arrayOf("v1")
-	}
+	/**
+	 * The supported API versions
+	 * @since 1.4.1
+	 */
+	val API_VERSIONS = arrayOf("v1")
 }
