22f6178653f8e9c0a4a6aab2c6e6926f66a6aa4a
termer <termer@termer.net>
2023-01-10 22:04:49 -0500

    add better rate limiting utils and rate limit auth

--- app/src/main/kotlin/net/termer/twinemedia/controller/AuthController.kt
+++ app/src/main/kotlin/net/termer/twinemedia/controller/AuthController.kt
@@ -3,12 +3,16 @@ package net.termer.twinemedia.controller
 import io.vertx.ext.web.RoutingContext
 import net.termer.krestx.api.util.ApiResponse
 import net.termer.krestx.api.util.apiSuccess
+import net.termer.twinemedia.AppContext
+import net.termer.twinemedia.service.RateLimitService
 
 /**
  * Controller for authentication/authorization operations
  * @since 2.0.0
  */
-class AuthController(override val ctx: RoutingContext) : Controller {
+class AuthController(override val appCtx: AppContext, override val ctx: RoutingContext) : Controller() {
+    private val cfg = appCtx.config
+
     override suspend fun initialize(): ApiResponse? {
         // Nothing to initialize
         return null
@@ -19,6 +23,18 @@ class AuthController(override val ctx: RoutingContext) : Controller {
      * @since 2.0.0
      */
     suspend fun auth(): ApiResponse {
+        // Rate limit request based on config values
+        val rlRes = RateLimitService.INSTANCE.rateLimitRequest(ctx, "AUTH", cfg.authTimeoutSeconds, cfg.authMaxFailedAttempts)
+        if (rlRes != null)
+            return rlRes
+
+        // Collect params
+        val bodyJson = bodyParams.jsonObject
+        val email = bodyJson.getString("email")
+        val password = bodyJson.getString("password")
+
+
+
         return apiSuccess()
     }
 }
--- app/src/main/kotlin/net/termer/twinemedia/service/RateLimitService.kt
+++ app/src/main/kotlin/net/termer/twinemedia/service/RateLimitService.kt
@@ -1,5 +1,11 @@
 package net.termer.twinemedia.service
 
+import io.vertx.ext.web.RoutingContext
+import net.termer.krestx.api.util.ApiErrorResponse
+import net.termer.krestx.api.util.apiError
+import net.termer.krestx.api.util.send
+import net.termer.twinemedia.util.validation.ip
+
 /**
  * Service for handling rate limiting
  * @since 2.0.0
@@ -32,18 +38,43 @@ class RateLimitService(
         }
     }
 
+    /**
+     * A rate limit preset.
+     * Contains all parameters for rate limiting functions, except for the accessor.
+     * @since 2.0.0
+     */
+    data class Preset(
+        /**
+         * The resource identifier (e.g. "LOGIN_PAGE")
+         * @since 2.0.0
+         */
+        val resource: String,
+
+        /**
+         * The rate limit time window, in seconds
+         * @since 2.0.0
+         */
+        val windowSeconds: Int,
+
+        /**
+         * The maximum number of hits allowed in within the rate limit time window
+         * @since 2.0.0
+         */
+        val windowMaxHits: Int
+    )
+
     private inline fun mkKey(resource: String, accessor: String) = "rl_${resource}_$accessor"
 
     /**
      * Records a hit on a resource by an accessor and returns whether the maximum number of hits for the specified window was reached
      * @param resource The resource identifier (e.g. "LOGIN_PAGE")
-     * @param accessor The accessor identifier (e.g. an IP address)
      * @param windowSeconds The rate limit time window, in seconds
      * @param windowMaxHits The maximum number of hits allowed in within the rate limit time window
+     * @param accessor The accessor identifier (e.g. an IP address)
      * @return Whether the maximum number of hits for the specified window was reached
      * @since 2.0.0
      */
-    suspend fun hitAndCheck(resource: String, accessor: String, windowSeconds: Int, windowMaxHits: Int): Boolean {
+    suspend fun hitAndCheck(resource: String, windowSeconds: Int, windowMaxHits: Int, accessor: String): Boolean {
         val key = mkKey(resource, accessor)
 
         val hits: Int
@@ -58,18 +89,77 @@ class RateLimitService(
     }
 
     /**
+     * Records a hit on a resource by an accessor and returns whether the maximum number of hits for the specified window was reached
+     * @param preset The rate limit preset to use
+     * @param accessor The accessor identifier (e.g. an IP address)
+     * @return Whether the maximum number of hits for the specified window was reached
+     * @since 2.0.0
+     */
+    suspend fun hitAndCheck(preset: Preset, accessor: String): Boolean {
+        return hitAndCheck(preset.resource, preset.windowSeconds, preset.windowMaxHits, accessor)
+    }
+
+    /**
      * Returns whether the maximum number of hits was reached.
      * Usually you should use [hitAndCheck] because it automatically records a hit.
      * Use this method only for operations that require you to not modify rate limit state.
      * @param resource The resource identifier (e.g. "LOGIN_PAGE")
-     * @param accessor The accessor identifier (e.g. an IP address)
      * @param windowMaxHits The maximum number of hits allowed
+     * @param accessor The accessor identifier (e.g. an IP address)
      * @return Whether the maximum number of hits was reached.
      * @since 2.0.0
      */
-    suspend fun check(resource: String, accessor: String, windowMaxHits: Int): Boolean {
+    suspend fun check(resource: String, windowMaxHits: Int, accessor: String): Boolean {
         val hits = redisService.getInt(mkKey(resource, accessor))
 
         return hits != null && hits >= windowMaxHits
+    }
+
+    /**
+     * Returns whether the maximum number of hits was reached.
+     * Usually you should use [hitAndCheck] because it automatically records a hit.
+     * Use this method only for operations that require you to not modify rate limit state.
+     * @param preset The rate limit preset to use
+     * @param accessor The accessor identifier (e.g. an IP address)
+     * @return Whether the maximum number of hits was reached.
+     * @since 2.0.0
+     */
+    suspend fun check(preset: Preset, accessor: String): Boolean {
+        return check(preset.resource, preset.windowMaxHits, accessor)
+    }
+
+    /**
+     * Rate limits a request by recording a hit on a resource, and return a "rate_limited" error if the maximum number of hits for the specified window was reached.
+     * Returns an error response, or null if the limit was not reached.
+     * If an error is returned, your controller should immediately return it, because headers are set on the request when an error is returned.
+     * @param ctx The request [RoutingContext]
+     * @param resource The resource identifier (e.g. "LOGIN_PAGE")
+     * @param windowSeconds The rate limit time window, in seconds
+     * @param windowMaxHits The maximum number of hits allowed in within the rate limit time window
+     * @param accessor The accessor identifier (e.g. an IP address, defaults to the request's IP address)
+     * @return An error response, or null if the limit was not reached
+     * @since 2.0.0
+     */
+    suspend fun rateLimitRequest(ctx: RoutingContext, resource: String, windowSeconds: Int, windowMaxHits: Int, accessor: String = ctx.ip()): ApiErrorResponse? {
+        return if (hitAndCheck(resource, windowSeconds, windowMaxHits, accessor)) {
+            ctx.response().putHeader("Retry-After", windowSeconds.toString())
+            return apiError("rate_limited", "Rate limited", statusCode = 429)
+        } else {
+            null
+        }
+    }
+
+    /**
+     * Rate limits a request by recording a hit on a resource, and return a "rate_limited" error if the maximum number of hits for the specified window was reached.
+     * Returns an error response, or null if the limit was not reached.
+     * If an error is returned, your controller should immediately return it, because headers are set on the request when an error is returned.
+     * @param ctx The request [RoutingContext]
+     * @param preset The rate limit preset to use
+     * @param accessor The accessor identifier (e.g. an IP address, defaults to the request's IP address)
+     * @return An error response, or null if the limit was not reached
+     * @since 2.0.0
+     */
+    suspend fun rateLimitRequest(ctx: RoutingContext, preset: Preset, accessor: String = ctx.ip()): ApiErrorResponse? {
+        return rateLimitRequest(ctx, preset.resource, preset.windowSeconds, preset.windowMaxHits, accessor)
     }
 }
