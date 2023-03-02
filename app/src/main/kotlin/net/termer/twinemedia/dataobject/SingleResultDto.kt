9818803427a29d704234b3dcc2d04d474aac3e19
termer <termer@termer.net>
2023-03-01 23:23:08 -0500

    support null result

--- app/src/main/kotlin/net/termer/twinemedia/dataobject/SingleResultDto.kt
+++ app/src/main/kotlin/net/termer/twinemedia/dataobject/SingleResultDto.kt
@@ -6,10 +6,11 @@ import net.termer.twinemedia.util.JsonSerializable
 /**
  * DTO for a single result.
  * Should be used to wrap responses that only contain one record.
+ * @param result The result (can be null)
  * @since 2.0.0
  */
 data class SingleResultDto<T : JsonSerializable>(
-    val result: T
+    val result: T?
 ) : JsonSerializable() {
-    override fun toJson() = jsonObjectOf("result" to result.toJson())
+    override fun toJson() = jsonObjectOf("result" to result?.toJson())
 }
